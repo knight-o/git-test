f45c6b9165f66dae75bbd017af7730627eddbb7b
zhangrui <1017797687@qq.com>
2021-02-21 15:55:09 +0800

    把本地工程添加到git

---
+++ hellogit/src/com/yitian/day01/HelloDemo1.java
@@ -0,0 +1,13 @@
+package com.yitian.day01;
+
+/**
+ * @Author: Zhang
+ * @Description:
+ * @Date: Created in 15:48 2021/2/21
+ * @Modified By:
+ */
+public class HelloDemo1 {
+    public static void main(String[] args) {
+        System.out.println("hello wrold");
+    }
+}
